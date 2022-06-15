4a237da7992075daed7a3a2ddfa65cc533167f3c
ldldz <qkrqudrjs4350@gmail.com>
2022-06-15 17:40:35 +0900

    Feat: 라우팅 페이지 추가
    Co-authored-by: YUNHO <kimyouknow@users.noreply.github.com>

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,20 @@
-import Card from '@/components/Card';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-function App() {
-  return <Card content="project" msg="start" />;
+import { IssueDetail, Issues, Labels, Login, Milestones, NotFound, Signup } from '@/pages';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/issues" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/issues" element={<Issues />} />
+        <Route path="/issues/:id" element={<IssueDetail />} />
+        <Route path="/labels" element={<Labels />} />
+        <Route path="/milestones" element={<Milestones />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
---
+++ fe/src/pages/index.ts
@@ -0,0 +1,9 @@
+import IssueDetail from './IssueDetail';
+import Issues from './Issues';
+import Labels from './Labels';
+import Login from './Login';
+import Milestones from './Milestones';
+import NotFound from './NotFound';
+import Signup from './Signup';
+
+export { IssueDetail, Issues, Labels, Login, Milestones, NotFound, Signup };
