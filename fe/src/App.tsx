7c6e02aaaf344925d79c992b7d5e8a7aa24e6562
ldldz <qkrqudrjs4350@gmail.com>
2022-06-16 16:05:23 +0900

    Feat: 임시로 사용할 isLogin 변수 추가

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -6,16 +6,29 @@ import { IssueDetail, Issues, Labels, Login, Milestones, NotFound, Signup } from
 
 // TODO: isLogin이라는 util 메서드를 만들어서 로그인 여부를 boolean으로 반환하기
 export default function App() {
+  const isLogin = false;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/issues" replace />} />
-        <Route path="/login" element={<PublicRoute Component={Login} isLogin restricted />} />
-        <Route path="/signup" element={<PublicRoute Component={Signup} isLogin restricted />} />
-        <Route path="/issues" element={<PrivateRoute Component={Issues} isLogin />} />
-        <Route path="/issues/:id" element={<PrivateRoute Component={IssueDetail} isLogin />} />
-        <Route path="/labels" element={<PrivateRoute Component={Labels} isLogin />} />
-        <Route path="/milestones" element={<PrivateRoute Component={Milestones} isLogin />} />
+        <Route
+          path="/login"
+          element={<PublicRoute Component={Login} isLogin={isLogin} restricted />}
+        />
+        <Route
+          path="/signup"
+          element={<PublicRoute Component={Signup} isLogin={isLogin} restricted />}
+        />
+        <Route path="/issues" element={<PrivateRoute Component={Issues} isLogin={isLogin} />} />
+        <Route
+          path="/issues/:id"
+          element={<PrivateRoute Component={IssueDetail} isLogin={isLogin} />}
+        />
+        <Route path="/labels" element={<PrivateRoute Component={Labels} isLogin={isLogin} />} />
+        <Route
+          path="/milestones"
+          element={<PrivateRoute Component={Milestones} isLogin={isLogin} />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
