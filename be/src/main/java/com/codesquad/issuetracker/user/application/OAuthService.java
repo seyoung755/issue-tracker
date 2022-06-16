6d71138fac9f997cef64af1940cbb9d5a16dc451
함석호 <kalejuice02@gmail.com>
2022-06-16 14:19:52 +0900

    refactor : Oauth 로그인 시 토큰이 있으면 바로 로그인 처리를 할 수 있도록 LoginInterceptor 구현 및 찾아온 유저 저장하는 로직 작성

--- be/src/main/java/com/codesquad/issuetracker/user/application/OAuthService.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/OAuthService.java
@@ -1,8 +1,11 @@
 package com.codesquad.issuetracker.user.application;
 
+import com.codesquad.issuetracker.auth.application.JwtProvider;
 import com.codesquad.issuetracker.user.application.dto.GithubAccessToken;
 import com.codesquad.issuetracker.user.application.dto.GithubTokenRequestDto;
 import com.codesquad.issuetracker.user.application.dto.GithubUserInformation;
+import com.codesquad.issuetracker.user.domain.User;
+import com.codesquad.issuetracker.user.domain.UserRepository;
 import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,25 +24,41 @@ public class OAuthService {
 
     private final String clientId;
     private final String clientSecret;
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
 
     public OAuthService(
             @Value("${oauth.github.client_id}") String clientId,
-            @Value("${oauth.github.client_secret}") String clientSecret) {
+            @Value("${oauth.github.client_secret}") String clientSecret,
+            UserRepository userRepository,
+            JwtProvider jwtProvider) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
     }
 
     public LoginResponseDto login(String code) {
 
         GithubTokenRequestDto githubTokenRequestDto = new GithubTokenRequestDto(clientId, clientSecret, code);
-
         log.debug("OAuthService, login: {}", githubTokenRequestDto);
 
-        GithubAccessToken accessToken = requestAccessToken(githubTokenRequestDto);
-        log.debug("OAuthService, access token: {}", accessToken.getAccessToken());
-        GithubUserInformation githubUserInformation = requestUserInformation(accessToken);
+        GithubAccessToken githubAccessToken = requestAccessToken(githubTokenRequestDto);
+        log.debug("OAuthService, access token: {}", githubAccessToken.getAccessToken());
 
-        return null;
+        GithubUserInformation githubUserInformation = requestUserInformation(githubAccessToken);
+
+        //db에 유저가 이미 있다면 로그인
+        //db에 유저가 없다면 저장 후 로그인
+        User user = userRepository.findByUserId("userId").orElseGet(()->{
+            User u = new User();
+            userRepository.save(u);
+            return u;
+        });
+
+        String accessToken = jwtProvider.createAccessToken(user.getId());
+        String refreshToken = jwtProvider.createRefreshToken(user.getId());
+        return new LoginResponseDto(accessToken, refreshToken);
     }
 
     private GithubAccessToken requestAccessToken(GithubTokenRequestDto githubTokenRequestDto) {
