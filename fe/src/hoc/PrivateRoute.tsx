3dfde1741bfcad8a688396eba03ddd6a3cfa18f6
yunho <kimyouknow@naver.com>
2022-06-16 11:15:22 +0900

    Refactor: NotAllow 범용성 갖추기
    warnMessage 외에도
    fallbackButtonMessage, fallbackPath를 props로 전달받기

--- fe/src/hoc/PrivateRoute.tsx
+++ fe/src/hoc/PrivateRoute.tsx
@@ -12,6 +12,10 @@ export default function PrivateRoute({ Component, isLogin }: PrivateRouteProps) 
   return isLogin ? (
     <Component />
   ) : (
-    <NotAllow warnMessage="로그인하지 않은 유저는 접근할 수 없다." />
+    <NotAllow
+      warnMessage="로그인하지 않은 유저는 접근할 수 없다."
+      fallbackButtonMessage={'로그인 페이지로 이동'}
+      fallbackPath={'/login'}
+    />
   );
 }
