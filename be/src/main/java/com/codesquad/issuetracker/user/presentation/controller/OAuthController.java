d8995de3abbea1509cde6d8acff3a35455ab4355
seyeong <seyoung7555@naver.com>
2022-06-17 14:20:20 +0900

    refactor: redirect url 수정

--- be/src/main/java/com/codesquad/issuetracker/user/presentation/controller/OAuthController.java
+++ be/src/main/java/com/codesquad/issuetracker/user/presentation/controller/OAuthController.java
@@ -3,32 +3,37 @@ package com.codesquad.issuetracker.user.presentation.controller;
 import com.codesquad.issuetracker.user.application.OAuthService;
 import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-@RequiredArgsConstructor
 @RequestMapping("/oauth/login")
 @RestController
 public class OAuthController {
 
-    private static final String GITHUB_REDIRECT_URL =
-            "https://github.com/login/oauth/authorize?client_id=fbfd3db42dc5b227d1b8&scope=user:email";
+
+    public OAuthController(@Value("${oauth.github.client_id") String clientId,
+                           OAuthService oAuthService) {
+        this.oAuthService = oAuthService;
+        redirectUrl =
+                "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=user:email";
+
+    }
 
     private final OAuthService oAuthService;
+    private final String redirectUrl;
 
     @Operation(summary = "Github 로그인 화면으로 Redirect 하기", description = "Github 로그인 페이지로 이동합니다.")
     @GetMapping()
     public ResponseEntity<Void> login() throws URISyntaxException {
-        /**
-         * todo: 이미 토큰을 들고 있을 때 유효한 토큰이면, redirect를 거치지 않고 바로 로그인 처리
-         * LoginInterceptor를 별도로 구현해서 유효할 경우 인터셉터에서 바로 false를 주고 요청을 끝내야할까?
-         */
-        return ResponseEntity.status(HttpStatus.FOUND).location(new URI(GITHUB_REDIRECT_URL)).build();
+        return ResponseEntity.status(HttpStatus.FOUND).location(new URI(redirectUrl)).build();
     }
 
     @Operation(summary = "Github 로그인 처리하기", description = "Github Authorization code 를 받아서 로그인을 완료합니다.")
