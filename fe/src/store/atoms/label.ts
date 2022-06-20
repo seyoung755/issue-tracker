0759b6e55f13b072daf9f3b5c74684a0d7e87019
yunho <kimyouknow@naver.com>
2022-06-20 22:38:27 +0900

    Feat: label mock 데이터

---
+++ fe/src/store/atoms/label.ts
@@ -0,0 +1,53 @@
+import { atom } from 'recoil';
+
+export const LABEL_ATOM_KEY = 'userState' as const;
+
+export interface LabelTypes {
+  labelName: string;
+  description: string;
+  colorCode: string;
+  textColor: string;
+}
+
+export interface LabelWithTotalCountTypes {
+  totalCount: number;
+  labels: LabelTypes[];
+}
+export const labelState = atom<LabelWithTotalCountTypes>({
+  key: LABEL_ATOM_KEY,
+  default: {
+    totalCount: 5,
+    labels: [
+      {
+        labelName: 'fe',
+        description: 'fe class',
+        colorCode: '#273a3e',
+        textColor: '#000000',
+      },
+      {
+        labelName: 'be',
+        description: 'be class',
+        colorCode: '#0f2337',
+        textColor: '#000000',
+      },
+      {
+        labelName: 'ios',
+        description: 'ios class',
+        colorCode: '#382628',
+        textColor: '#000000',
+      },
+      {
+        labelName: 'android',
+        description: 'android class',
+        colorCode: '#d876e3',
+        textColor: '#000000',
+      },
+      {
+        labelName: 'etc',
+        description: 'etc...',
+        colorCode: '#2b3510',
+        textColor: '#000000',
+      },
+    ],
+  },
+});
