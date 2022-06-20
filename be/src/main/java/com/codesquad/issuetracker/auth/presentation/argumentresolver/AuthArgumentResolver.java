3f5cc0d0de995d126264a397b8c63b8715506100
함석호 <kalejuice02@gmail.com>
2022-06-21 00:13:58 +0900

    refactor : AuthInterceptor, ArgumentResolver에 @Component 어노테이션 추가 및 config 파일에서 빈객체 생성하는 메서드 제거

--- be/src/main/java/com/codesquad/issuetracker/auth/presentation/argumentresolver/AuthArgumentResolver.java
+++ be/src/main/java/com/codesquad/issuetracker/auth/presentation/argumentresolver/AuthArgumentResolver.java
@@ -1,6 +1,7 @@
 package com.codesquad.issuetracker.auth.presentation.argumentresolver;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -8,15 +9,13 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Component
 public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
 
-//        해당 파라미터에 Auth 어노테이션이 붙어 있는가?
         boolean hasAuth = parameter.hasParameterAnnotation(Auth.class);
-
-//        해당 파라미터의 타입이 User 타입인가?
         boolean hasUser = com.codesquad.issuetracker.user.domain.User.class.isAssignableFrom(parameter.getParameterType());
 
         return hasAuth && hasUser;
@@ -25,7 +24,6 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
-        //httpServletRequest를 사용하기 위한 작업
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
 
         return request.getAttribute("user");
