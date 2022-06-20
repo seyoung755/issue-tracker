27affb694795f78f5e77838423e5d179e97fd797
seyeong <seyoung7555@naver.com>
2022-06-20 21:49:48 +0900

    feat: access token 검증 로직 추가

--- be/src/main/java/com/codesquad/issuetracker/auth/presentation/interceptor/AuthInterceptor.java
+++ be/src/main/java/com/codesquad/issuetracker/auth/presentation/interceptor/AuthInterceptor.java
@@ -1,6 +1,11 @@
 package com.codesquad.issuetracker.auth.presentation.interceptor;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.codesquad.issuetracker.auth.application.AuthService;
+import com.codesquad.issuetracker.exception.domain.type.AuthExceptionType;
+import com.codesquad.issuetracker.exception.domain.BusinessException;
 import com.codesquad.issuetracker.user.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 public class AuthInterceptor implements HandlerInterceptor {
+
+    private static final String TOKEN_TYPE = "Bearer ";
 
     private final AuthService authService;
 
@@ -23,10 +30,39 @@ public class AuthInterceptor implements HandlerInterceptor {
         log.debug("request-uri: {}", request.getRequestURI());
 
         String authorization = request.getHeader("Authorization");
-        User user = authService.checkTokenAndFindUser(authorization);
+        String token = parseToken(authorization);
 
+        try {
+            authService.validateToken(token);
+        } catch (TokenExpiredException e) {
+            e.printStackTrace();
+            throw new BusinessException(AuthExceptionType.TOKEN_EXPIRED);
+        } catch (SignatureVerificationException | JWTDecodeException e) {
+            e.printStackTrace();
+            throw new BusinessException(AuthExceptionType.INVALID_TOKEN);
+        }
+
+        User user = authService.findUser(token);
         request.setAttribute("user", user);
 
         return true;
+    }
+
+    private String parseToken(String authorization) {
+        String token;
+        try {
+            validateAuthorizationHeader(authorization);
+            token = authorization.split(" ")[1].trim();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            e.printStackTrace();
+            throw new BusinessException(AuthExceptionType.TOKEN_NOT_FOUND);
+        }
+        return token;
+    }
+
+    private void validateAuthorizationHeader(String authorization) {
+        if (authorization == null || !authorization.startsWith(TOKEN_TYPE)) {
+            throw new IllegalArgumentException("토큰의 형식이 잘못되었습니다.");
+        }
     }
 }
