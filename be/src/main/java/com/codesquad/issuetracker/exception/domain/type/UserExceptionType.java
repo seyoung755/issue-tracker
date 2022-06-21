f9d7f2792c79cc566a01c8f771e11fdce2971e55
seyeong <seyoung7555@naver.com>
2022-06-21 18:04:36 +0900

    feat: 회원 로그인 시 비밀번호 불일치 예외 정의

--- be/src/main/java/com/codesquad/issuetracker/exception/domain/type/UserExceptionType.java
+++ be/src/main/java/com/codesquad/issuetracker/exception/domain/type/UserExceptionType.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum UserExceptionType implements ExceptionType {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
 
     private final HttpStatus statusCode;
     private final String message;
