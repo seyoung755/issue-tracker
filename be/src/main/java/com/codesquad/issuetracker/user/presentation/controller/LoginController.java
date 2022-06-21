9f9debe67387fd211cbb6ee30f388a8d29056fcf
seyeong <seyoung7555@naver.com>
2022-06-21 18:00:51 +0900

    refactor: Login 관련 controller 통합

--- be/src/main/java/com/codesquad/issuetracker/user/presentation/controller/LoginController.java
+++ be/src/main/java/com/codesquad/issuetracker/user/presentation/controller/LoginController.java
@@ -1,44 +1,44 @@
 package com.codesquad.issuetracker.user.presentation.controller;
 
-import com.codesquad.issuetracker.user.application.OAuthService;
+import com.codesquad.issuetracker.user.application.BasicLoginService;
+import com.codesquad.issuetracker.user.application.OAuthLoginService;
 import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-@RequestMapping("/oauth/login")
 @RestController
-public class OAuthController {
+public class LoginController {
 
-
-    public OAuthController(@Value("${oauth.github.client_id}") String clientId,
-                           OAuthService oAuthService) {
-        this.oAuthService = oAuthService;
-        redirectUrl =
-                "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=user:email";
-
-    }
-
-    private final OAuthService oAuthService;
+    private final OAuthLoginService oAuthLoginService;
+    private final BasicLoginService basicLoginService;
     private final String redirectUrl;
 
+    public LoginController(@Value("${oauth.github.client_id}") String clientId,
+                           OAuthLoginService oAuthLoginService,
+                           BasicLoginService basicLoginService) {
+        this.oAuthLoginService = oAuthLoginService;
+        this.basicLoginService = basicLoginService;
+        redirectUrl =
+                "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=user:email";
+    }
+
     @Operation(summary = "Github 로그인 화면으로 Redirect 하기", description = "Github 로그인 페이지로 이동합니다.")
-    @GetMapping()
+    @GetMapping("/oauth/github/login")
     public ResponseEntity<Void> login() throws URISyntaxException {
         return ResponseEntity.status(HttpStatus.FOUND).location(new URI(redirectUrl)).build();
     }
 
     @Operation(summary = "Github 로그인 처리하기", description = "Github Authorization code 를 받아서 로그인을 완료합니다.")
-    @GetMapping("/callback")
+    @GetMapping("/oauth/github/callback")
     public LoginResponseDto receiveCallback(@RequestParam String code) {
-        return oAuthService.login(code);
+        return oAuthLoginService.githubLogin(code);
     }
 }
