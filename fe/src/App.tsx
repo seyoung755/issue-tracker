cd41f7bf3eb5da6efc7bd161746cf4708c6fe53e
ldldz <qkrqudrjs4350@gmail.com>
2022-06-21 14:28:53 +0900

    Feat: TextInput 컴포넌트 테스트용 TextInputs 페이지 작성

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,7 +2,17 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import PrivateRoute from '@/hoc/PrivateRoute';
 import PublicRoute from '@/hoc/PublicRoute';
-import { Buttons, IssueDetail, Issues, Labels, Login, Milestones, NotFound, Signup } from '@/pages';
+import {
+  Buttons,
+  IssueDetail,
+  Issues,
+  Labels,
+  Login,
+  Milestones,
+  NotFound,
+  Signup,
+  TextInputs,
+} from '@/pages';
 
 // TODO: isLogin이라는 util 메서드를 만들어서 로그인 여부를 boolean으로 반환하기
 export default function App() {
@@ -12,6 +22,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/issues" replace />} />
         <Route path="/buttons" element={<Buttons />} />
+        <Route path="/textInputs" element={<TextInputs />} />
+
         <Route
           path="/login"
           element={<PublicRoute Component={Login} isLogin={isLogin} restricted />}
