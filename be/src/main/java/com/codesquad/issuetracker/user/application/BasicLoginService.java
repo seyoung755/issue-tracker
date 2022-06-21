72dbe57062c3547c5cdf359b0c39074952da0dd0
seyeong <seyoung7555@naver.com>
2022-06-21 18:03:50 +0900

    refactor: 일반 로그인과 OAuth 로그인 서비스 분리 및 OAuth 서비스 추상화

---
+++ be/src/main/java/com/codesquad/issuetracker/user/application/BasicLoginService.java
@@ -0,0 +1,9 @@
+package com.codesquad.issuetracker.user.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class BasicLoginService {
+}
--- be/src/main/java/com/codesquad/issuetracker/user/application/oauth/GithubLoginService.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/oauth/GithubLoginService.java
@@ -1,12 +1,8 @@
-package com.codesquad.issuetracker.user.application;
+package com.codesquad.issuetracker.user.application.oauth;
 
-import com.codesquad.issuetracker.auth.application.JwtProvider;
 import com.codesquad.issuetracker.user.application.dto.GithubAccessToken;
 import com.codesquad.issuetracker.user.application.dto.GithubTokenRequestDto;
 import com.codesquad.issuetracker.user.application.dto.GithubUserInformation;
-import com.codesquad.issuetracker.user.domain.User;
-import com.codesquad.issuetracker.user.domain.UserRepository;
-import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -17,29 +13,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Slf4j
 @Transactional
 @Service
-public class OAuthService {
+public class GithubLoginService implements OAuthProvider {
 
     private static final String GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
     private static final String GITHUB_RESOURCE_URL = "https://api.github.com/user";
 
     private final String clientId;
     private final String clientSecret;
-    private final JwtProvider jwtProvider;
-    private final UserRepository userRepository;
 
-    public OAuthService(
+    public GithubLoginService(
             @Value("${oauth.github.client_id}") String clientId,
-            @Value("${oauth.github.client_secret}") String clientSecret,
-            UserRepository userRepository,
-            JwtProvider jwtProvider) {
+            @Value("${oauth.github.client_secret}") String clientSecret) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
-        this.userRepository = userRepository;
-        this.jwtProvider = jwtProvider;
     }
 
-    public LoginResponseDto login(String code) {
-
+    @Override
+    public GithubUserInformation requestUserInformation(String code) {
         GithubTokenRequestDto githubTokenRequestDto = new GithubTokenRequestDto(clientId, clientSecret, code);
         log.debug("OAuthService, login: {}", githubTokenRequestDto);
 
@@ -49,24 +39,7 @@ public class OAuthService {
         GithubUserInformation githubUserInformation = requestUserInformation(githubAccessToken);
         log.debug("OAuthService, github user information: {}", githubUserInformation);
 
-        //db에 유저가 이미 있다면 로그인
-        //db에 유저가 없다면 저장 후 로그인
-        User user = userRepository.findByUserId(githubUserInformation.getUserId())
-                .orElseGet(() -> {
-                    User u = new User(
-                            githubUserInformation.getUserId(),
-                            githubUserInformation.getUsername(),
-                            githubUserInformation.getPassword(),
-                            githubUserInformation.getProfileImage()
-                    );
-                    userRepository.save(u);
-                    return u;
-                });
-
-        String accessToken = jwtProvider.createAccessToken(user.getId());
-        // TODO: 2022/06/17 user에 refresh token 저장하는 로직 추가해야함.
-        String refreshToken = jwtProvider.createRefreshToken(user.getId());
-        return new LoginResponseDto(accessToken, refreshToken);
+        return githubUserInformation;
     }
 
     private GithubAccessToken requestAccessToken(GithubTokenRequestDto githubTokenRequestDto) {
@@ -96,8 +69,14 @@ public class OAuthService {
                 .block();
 
         log.debug("OAuthService, requestUserInfo.: {}", userInformation);
+        String username = generateUsername(userInformation.getUsername());
+        userInformation.setUsername(username);
 
         return userInformation;
+    }
+
+    private String generateUsername(String username) {
+        return "GITHUB_" + username;
     }
 
     private String createTokenString(GithubAccessToken githubAccessToken) {
