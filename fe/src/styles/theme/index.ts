50d0d668b5dfaa982fee4d359373bafbf29ee31f
ldldz <qkrqudrjs4350@gmail.com>
2022-06-21 14:27:08 +0900

    Feat: TextInputProps type 설정

--- fe/src/styles/theme/index.ts
+++ fe/src/styles/theme/index.ts
@@ -4,15 +4,18 @@ import { buttonSizes } from './button';
 import { colors } from './color';
 import { fonts } from './font';
 import { iconSizes } from './icon';
+import { textInputSizes } from './textInput';
 
 export type ColorsTypes = typeof colors;
 export type FontTypes = typeof fonts;
 export type IconSizeTypes = typeof iconSizes;
 export type ButtonSizeTypes = typeof buttonSizes;
+export type TextInputSizes = typeof textInputSizes;
 
 export const theme: DefaultTheme = {
   colors,
   fonts,
   iconSizes,
   buttonSizes,
+  textInputSizes,
 };
---
+++ fe/src/styles/theme/textInput.ts
@@ -0,0 +1,19 @@
+import { css } from 'styled-components';
+
+export const textInputSizes = {
+  large: css`
+    width: 340px;
+    height: 64px;
+    border-radius: 16px;
+  `,
+  medium: css`
+    width: 320px;
+    height: 56px;
+    border-radius: 14px;
+  `,
+  small: css`
+    width: 300px;
+    height: 40px;
+    border-radius: 11px;
+  `,
+};
