5e1126f2fbf50be1c80315567cb46a1513287f24
seyeong <seyoung7555@naver.com>
2022-06-21 18:02:47 +0900

    feat: User 저장 시 가입 경로 및 refresh token을 저장하도록 수정

--- be/src/main/java/com/codesquad/issuetracker/user/presentation/controller/UserController.java
+++ be/src/main/java/com/codesquad/issuetracker/user/presentation/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.codesquad.issuetracker.user.presentation.controller;
 
 import com.codesquad.issuetracker.user.application.UserService;
+import com.codesquad.issuetracker.user.domain.LoginType;
 import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import com.codesquad.issuetracker.user.presentation.dto.UserJoinRequestDto;
@@ -29,14 +30,14 @@ public class UserController {
     @Operation(summary = "유저 회원가입하기", description = "새로운 사용자를 등록합니다.")
     @PostMapping("/join")
     public ResponseEntity<Void> join(@Validated @RequestBody UserJoinRequestDto userJoinRequestDto) {
-        userService.join(userJoinRequestDto);
+        userService.join(userJoinRequestDto, LoginType.NORMAL);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "유저 Id의 중복 여부 검사하기", description = "유저 Id의 중복 여부를 검사합니다.")
     @PostMapping("/join/checkId")
     public ResponseEntity<Boolean> checkDuplicateId(@RequestBody String userId) {
-        return ResponseEntity.ok().body(userService.isDuplicatedUserId(userId));
+        return ResponseEntity.ok().body(userService.isDuplicatedUsername(userId));
     }
 
 }
