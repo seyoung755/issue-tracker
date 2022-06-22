683fc4a8a111abba8b8cd5253da11f4a42a12d5b
seyeong <seyoung7555@naver.com>
2022-06-22 11:44:52 +0900

    refactor: 일반 로그인 분리

--- be/src/main/java/com/codesquad/issuetracker/user/application/BasicLoginService.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/BasicLoginService.java
@@ -1,9 +1,30 @@
 package com.codesquad.issuetracker.user.application;
 
+import com.codesquad.issuetracker.auth.application.JwtProvider;
+import com.codesquad.issuetracker.user.domain.User;
+import com.codesquad.issuetracker.user.domain.UserRepository;
+import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
+import com.codesquad.issuetracker.user.presentation.dto.UserLoginRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Transactional
 @Service
 public class BasicLoginService {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+
+    public LoginResponseDto login(UserLoginRequestDto userLoginRequestDto) {
+        User user = userRepository.findByUsername(userLoginRequestDto.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException(""));
+
+        user.validatePassword(userLoginRequestDto.getPassword());
+
+        String accessToken = jwtProvider.createAccessToken(user.getId());
+        String refreshToken = jwtProvider.createRefreshToken(user.getId());
+        return new LoginResponseDto(accessToken, refreshToken);
+    }
 }
