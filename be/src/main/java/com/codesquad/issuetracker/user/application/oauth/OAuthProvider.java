bf8bde1d848e55a261e136520f0aaa584d76e9ea
seyeong <seyoung7555@naver.com>
2022-06-22 10:58:29 +0900

    feat: google oauth 로그인 컨트롤러 및 서비스 구현

--- be/src/main/java/com/codesquad/issuetracker/user/application/oauth/OAuthProvider.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/oauth/OAuthProvider.java
@@ -1,10 +1,10 @@
 package com.codesquad.issuetracker.user.application.oauth;
 
-import com.codesquad.issuetracker.user.application.dto.GithubUserInformation;
+import com.codesquad.issuetracker.user.application.dto.OAuthUserInformation;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OAuthProvider {
 
-    GithubUserInformation requestUserInformation(String code);
+    OAuthUserInformation requestUserInformation(String code);
 }
