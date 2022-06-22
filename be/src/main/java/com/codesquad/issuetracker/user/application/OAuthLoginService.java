a50dd313fc2e97cebf597491ce23b0e9d3aef4ab
seyeong <seyoung7555@naver.com>
2022-06-22 10:58:29 +0900

    feat: google oauth 로그인 컨트롤러 및 서비스 구현

--- be/src/main/java/com/codesquad/issuetracker/user/application/OAuthLoginService.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/OAuthLoginService.java
@@ -1,8 +1,9 @@
 package com.codesquad.issuetracker.user.application;
 
 import com.codesquad.issuetracker.auth.application.JwtProvider;
-import com.codesquad.issuetracker.user.application.dto.GithubUserInformation;
+import com.codesquad.issuetracker.user.application.dto.OAuthUserInformation;
 import com.codesquad.issuetracker.user.application.oauth.GithubLoginService;
+import com.codesquad.issuetracker.user.application.oauth.GoogleLoginService;
 import com.codesquad.issuetracker.user.application.oauth.OAuthProvider;
 import com.codesquad.issuetracker.user.domain.User;
 import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
@@ -20,15 +21,20 @@ public class OAuthLoginService {
     private final JwtProvider jwtProvider;
     private final UserService userService;
     private final GithubLoginService githubLoginService;
+    private final GoogleLoginService googleLoginService;
 
     public LoginResponseDto githubLogin(String code) {
         return login(code, githubLoginService);
     }
 
-    public LoginResponseDto login(String code, OAuthProvider oAuthProvider) {
-        GithubUserInformation githubUserInformation = oAuthProvider.requestUserInformation(code);
+    public LoginResponseDto googleLogin(String code) {
+        return login(code, googleLoginService);
+    }
 
-        User user = userService.oAuthLogin(githubUserInformation);
+    public LoginResponseDto login(String code, OAuthProvider oAuthProvider) {
+        OAuthUserInformation oAuthUserInformation = oAuthProvider.requestUserInformation(code);
+
+        User user = userService.oAuthLogin(oAuthUserInformation);
 
         String accessToken = jwtProvider.createAccessToken(user.getId());
         String refreshToken = jwtProvider.createRefreshToken(user.getId());
