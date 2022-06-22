b3f321de2a4d0804af56fa053a02bbaebbfe1665
seyeong <seyoung7555@naver.com>
2022-06-22 11:43:41 +0900

    style: userId를 username으로 변경

--- be/src/main/java/com/codesquad/issuetracker/user/domain/UserRepository.java
+++ be/src/main/java/com/codesquad/issuetracker/user/domain/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String userId);
+    Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
 }
