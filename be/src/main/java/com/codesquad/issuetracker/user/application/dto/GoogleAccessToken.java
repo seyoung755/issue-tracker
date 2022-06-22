0048a34f5219ee0424a493f752f56f52ba94dad3
seyeong <seyoung7555@naver.com>
2022-06-22 10:55:49 +0900

    feat: google oauth 로그인에 필요한 dto 생성

---
+++ be/src/main/java/com/codesquad/issuetracker/user/application/dto/GoogleAccessToken.java
@@ -0,0 +1,14 @@
+package com.codesquad.issuetracker.user.application.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Getter
+public class GoogleAccessToken {
+
+    private String accessToken;
+    private String scope;
+    private String tokenType;
+}
