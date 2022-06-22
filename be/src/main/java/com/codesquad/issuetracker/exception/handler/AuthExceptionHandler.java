5c02f9252f90a4ec05f84d8913607fb497628914
함석호 <kalejuice02@gmail.com>
2022-06-22 17:49:22 +0900

    refactor : 예외처리 리팩토링
    - 자체 예외코드 추가
    - ExceptionHandler의 반환 타입을 ExceptionResponseDto로 변경

--- be/src/main/java/com/codesquad/issuetracker/exception/handler/AuthExceptionHandler.java
+++ be/src/main/java/com/codesquad/issuetracker/exception/handler/AuthExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.codesquad.issuetracker.exception.handler;
 
 import com.codesquad.issuetracker.exception.domain.BusinessException;
-import org.springframework.http.HttpStatus;
+import com.codesquad.issuetracker.exception.domain.type.ExceptionType;
+import com.codesquad.issuetracker.exception.domain.type.UserExceptionType;
+import com.codesquad.issuetracker.exception.dto.ExceptionResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,12 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class AuthExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> exceptionHandler(HttpServletRequest request, MethodArgumentNotValidException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("아이디나 비밀번호 형식이 잘못 되었습니다.");
+    public ResponseEntity<ExceptionResponseDto> exceptionHandler(HttpServletRequest request, MethodArgumentNotValidException exception) {
+        UserExceptionType exceptionType = UserExceptionType.INVALID_FORMAT;
+        return createResponseEntity(exceptionType);
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<String> handleBusinessException(HttpServletRequest request, BusinessException exception) {
-        return ResponseEntity.status(exception.getExceptionType().getStatusCode()).body(exception.getMessage());
+    public ResponseEntity<ExceptionResponseDto> handleBusinessException(HttpServletRequest request, BusinessException exception) {
+        return createResponseEntity(exception.getExceptionType());
+    }
+
+    private ResponseEntity<ExceptionResponseDto> createResponseEntity(ExceptionType exceptionType) {
+        return ResponseEntity.status(exceptionType.getStatusCode()).body(ExceptionResponseDto.from(exceptionType));
     }
 }
