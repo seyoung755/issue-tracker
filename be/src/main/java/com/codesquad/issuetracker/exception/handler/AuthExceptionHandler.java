306bd47f978da9a3b05da725900963d5c45593f0
seyeong <seyoung7555@naver.com>
2022-06-22 11:44:32 +0900

    feat: 검증 실패 시 발생하는 예외 핸들링

--- be/src/main/java/com/codesquad/issuetracker/exception/handler/AuthExceptionHandler.java
+++ be/src/main/java/com/codesquad/issuetracker/exception/handler/AuthExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.codesquad.issuetracker.exception.handler;
 
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.codesquad.issuetracker.exception.domain.BusinessException;
-import com.codesquad.issuetracker.exception.domain.type.AuthExceptionType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,21 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class AuthExceptionHandler {
-//
-//    @ExceptionHandler({IllegalArgumentException.class})
-//    public ResponseEntity<ExceptionResponseDto> exceptionHandler(HttpServletRequest request, IllegalArgumentException ex) {
-//        return ResponseEntity.ok().body(new ExceptionResponseDto(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
-//    }
-//
-//    @ExceptionHandler({SignatureVerificationException.class})
-//    public ResponseEntity<ExceptionResponseDto> exceptionHandler(HttpServletRequest request, SignatureVerificationException ex) {
-//        return ResponseEntity.ok().body(new ExceptionResponseDto(HttpStatus.UNAUTHORIZED.value(), ex.getMessage()));
-//    }
-
-    @ExceptionHandler(TokenExpiredException.class)
-    public ResponseEntity<String> exceptionHandler(HttpServletRequest request, TokenExpiredException exception) {
-        AuthExceptionType exceptionType = AuthExceptionType.TOKEN_NOT_FOUND;
-        return ResponseEntity.status(exceptionType.getStatusCode()).body(exceptionType.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> exceptionHandler(HttpServletRequest request, MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("아이디나 비밀번호 형식이 잘못 되었습니다.");
     }
 
     @ExceptionHandler(BusinessException.class)
