d94f27b52d7ab18df4be2ae6959007c7dcd613ec
ldldz <qkrqudrjs4350@gmail.com>
2022-06-24 12:20:48 +0900

    [FE] issueDetail atom 분리 (#39)
    * Feat: type 설정
    
    - issue, comment, label, milestone, user type 추가
    
    Co-authored-by: YUNHO <kimyouknow@naver.com>
    
    * Feat: issueDetail atom 분리
    
    - info, assginee, author, milestone, comments atom 분리
    
    Co-authored-by: YUNHO <kimyouknow@naver.com>

--- fe/src/stores/atoms/milestone.ts
+++ fe/src/stores/atoms/milestone.ts
@@ -1,22 +1,10 @@
 import { atom } from 'recoil';
 
+import { MilestoneListType } from '@/types/milestoneType';
+
 export const MILESTONE_ATOM_KEY = 'milestoneState' as const;
 
-export interface MilestoneTypes {
-  name: string;
-  dueDate: string; // TODO: yyyy-mm-dd 커스텀 타입으로 변경하기
-  description: string;
-  progressRate: number;
-  openCount: number;
-  closeCount: number;
-}
-
-export interface MilestoneListTypes {
-  totalCount: number;
-  milestones: MilestoneTypes[];
-}
-
-export const milestoneState = atom<MilestoneListTypes | null | undefined>({
+export const milestoneState = atom<MilestoneListType | null | undefined>({
   key: MILESTONE_ATOM_KEY,
   default: null,
 });
