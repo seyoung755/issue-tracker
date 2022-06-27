5c29deeed7623bcd8bba104e4155013f7e2aedc2
yunho <kimyouknow@naver.com>
2022-06-27 12:28:58 +0900

    Feat: forwardRef를 사용해서 button컴포넌트에 ref사용가능하게 하기

--- fe/src/components/Common/Button/index.tsx
+++ fe/src/components/Common/Button/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef } from 'react';
 import { CSSProp } from 'styled-components';
 
 import * as S from './style';
@@ -12,37 +12,35 @@ export interface ButtonProps {
   customStyle?: CSSProp | null | undefined;
 }
 
-export function Button({
-  children,
-  onClick,
-  disabled = false,
-  isSecondary = false,
-  className,
-  customStyle,
-}: ButtonProps) {
-  const Style = isSecondary ? S.Secondary : S.Button;
-  return (
-    <Style className={className} onClick={onClick} disabled={disabled} customStyle={customStyle}>
-      {children}
-    </Style>
-  );
-}
+export const Button = forwardRef(
+  ({
+    children,
+    onClick,
+    disabled = false,
+    isSecondary = false,
+    className,
+    customStyle,
+  }: ButtonProps) => {
+    const Style = isSecondary ? S.Secondary : S.Button;
+    return (
+      <Style className={className} onClick={onClick} disabled={disabled} customStyle={customStyle}>
+        {children}
+      </Style>
+    );
+  },
+);
 
-export function TextButton({
-  children,
-  onClick,
-  disabled = false,
-  className,
-  customStyle,
-}: ButtonProps) {
-  return (
-    <S.TextButton
-      className={className}
-      onClick={onClick}
-      disabled={disabled}
-      customStyle={customStyle}
-    >
-      {children}
-    </S.TextButton>
-  );
-}
+export const TextButton = forwardRef(
+  ({ children, onClick, disabled = false, className, customStyle }: ButtonProps) => {
+    return (
+      <S.TextButton
+        className={className}
+        onClick={onClick}
+        disabled={disabled}
+        customStyle={customStyle}
+      >
+        {children}
+      </S.TextButton>
+    );
+  },
+);
