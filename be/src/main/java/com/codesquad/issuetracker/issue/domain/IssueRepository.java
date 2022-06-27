b7478a6078b52ea44dfd625d35d3e81a21a5edb7
seyeong <seyoung7555@naver.com>
2022-06-27 21:41:09 +0900

    feat: Issue 제목, 내용 수정 및 삭제 api 구현

--- be/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
+++ be/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
@@ -2,5 +2,8 @@ package com.codesquad.issuetracker.issue.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    Optional<Issue> findByIdAndIsDeleted(long issueId, boolean b);
 }
