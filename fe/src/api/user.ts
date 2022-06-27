35d2988d48eb5123347a8915016b6c60a552c954
yunho <kimyouknow@naver.com>
2022-06-27 13:45:03 +0900

    Chore: userApi requestGithubOAuth() auth api로 분리해야하는 주석 작성

--- fe/src/api/user.ts
+++ fe/src/api/user.ts
@@ -2,6 +2,7 @@ import instance from '@/api/core';
 import { USER_API } from '@/constant/api';
 
 const userApi = {
+  // TODO: auth api로 분리하기
   requestGithubOAuth() {
     return instance({
       url: USER_API.GITHUB_OAUTH,
