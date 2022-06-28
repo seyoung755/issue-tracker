ffb08ca1a6883d6750f4024ebe095056bf221f54
yunho <kimyouknow@naver.com>
2022-06-28 16:16:26 +0900

    Rename: DropdownTextButtonList 구조변경

--- fe/src/pages/Issues/IssueTableHeader/index.tsx
+++ fe/src/pages/Issues/IssueTableHeader/index.tsx
@@ -1,29 +1,10 @@
-import DropdownContainer from './DropdownContainer';
-import LabelDropdown from './DropdownContainer/LabelDropdown';
-import MilestoneDropdown from './DropdownContainer/MilestoneDropdown';
-import UserDropdown from './DropdownContainer/UserDropdown';
+import DropdownTextButtonList from './DropdownTextButtonList';
 import * as S from './style';
-
-const DropdownTextButtonList = [
-  { id: 'assignee', header: '담당자', DropdownComponent: UserDropdown },
-  { id: 'label', header: '레이블', DropdownComponent: LabelDropdown },
-  { id: 'milestone', header: '마일스톤', DropdownComponent: MilestoneDropdown },
-  { id: 'writer', header: '작성자', DropdownComponent: UserDropdown },
-];
 
 export default function IssueTableHeader() {
   return (
     <S.IssueTableHeader>
-      <S.DropdownTextButtonContainer>
-        {DropdownTextButtonList.map(({ id, header, DropdownComponent }) => (
-          <DropdownContainer
-            key={id}
-            id={id}
-            header={header}
-            DropdownComponent={DropdownComponent}
-          />
-        ))}
-      </S.DropdownTextButtonContainer>
+      <DropdownTextButtonList />
     </S.IssueTableHeader>
   );
 }
