53d73be658f629d64ccff28c9deb068e5673e387
함석호 <kalejuice02@gmail.com>
2022-06-28 10:51:38 +0900

    refactor : 로그인 api 추상화

--- be/src/main/java/com/codesquad/issuetracker/user/application/OAuthLoginService.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/OAuthLoginService.java
@@ -2,9 +2,10 @@ package com.codesquad.issuetracker.user.application;
 
 import com.codesquad.issuetracker.auth.application.JwtProvider;
 import com.codesquad.issuetracker.user.application.dto.OAuthUserInformation;
-import com.codesquad.issuetracker.user.application.oauth.GithubLoginService;
-import com.codesquad.issuetracker.user.application.oauth.GoogleLoginService;
+import com.codesquad.issuetracker.user.application.oauth.GithubLoginProvider;
+import com.codesquad.issuetracker.user.application.oauth.GoogleLoginProvider;
 import com.codesquad.issuetracker.user.application.oauth.OAuthProvider;
+import com.codesquad.issuetracker.user.domain.LoginType;
 import com.codesquad.issuetracker.user.domain.User;
 import com.codesquad.issuetracker.user.presentation.dto.LoginResponseDto;
 import com.codesquad.issuetracker.user.presentation.dto.UserJoinRequestDto;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.EnumMap;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,18 +24,16 @@ public class OAuthLoginService {
 
     private final JwtProvider jwtProvider;
     private final UserService userService;
-    private final GithubLoginService githubLoginService;
-    private final GoogleLoginService googleLoginService;
 
-    public LoginResponseDto githubLogin(String code) {
-        return login(code, githubLoginService);
+    private final EnumMap<LoginType, OAuthProvider> oAuthProviderEnumMap;
+
+    public String getRedirectUrl(LoginType type) {
+        OAuthProvider oAuthProvider = oAuthProviderEnumMap.get(type);
+        return oAuthProvider.getRedirectUrl();
     }
 
-    public LoginResponseDto googleLogin(String code) {
-        return login(code, googleLoginService);
-    }
-
-    public LoginResponseDto login(String code, OAuthProvider oAuthProvider) {
+    public LoginResponseDto login(String code, LoginType type) {
+        OAuthProvider oAuthProvider = oAuthProviderEnumMap.get(type);
         OAuthUserInformation oAuthUserInformation = oAuthProvider.requestUserInformation(code);
 
         User user = oAuthLogin(oAuthUserInformation);
@@ -45,7 +46,7 @@ public class OAuthLoginService {
         return new LoginResponseDto(accessToken, refreshToken);
     }
 
-    public User oAuthLogin(OAuthUserInformation oAuthUserInformation) {
+    private User oAuthLogin(OAuthUserInformation oAuthUserInformation) {
         return userService.findByUsername(oAuthUserInformation.getUsername())
                 .orElseGet(() -> userService.join(UserJoinRequestDto.from(oAuthUserInformation)));
     }
