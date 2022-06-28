26d0fc12a775f2016576409cab58736cd514ac70
함석호 <kalejuice02@gmail.com>
2022-06-28 17:15:45 +0900

    refactor : 마일스톤 조회 시 발생하는 n+1 문제 해결

--- be/src/main/java/com/codesquad/issuetracker/milestone/domain/MilestoneRepository.java
+++ be/src/main/java/com/codesquad/issuetracker/milestone/domain/MilestoneRepository.java
@@ -1,9 +1,14 @@
 package com.codesquad.issuetracker.milestone.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
     Optional<Milestone> findByNameAndIsDeleted(String milestoneName, boolean b);
+
+    @Query("select distinct m from Milestone m join fetch m.issues where m.isDeleted=false")
+    List<Milestone> findAll();
 }
