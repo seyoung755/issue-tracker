2b532701c3ddcb0cb743b0adbd89b4c430a07557
함석호 <kalejuice02@gmail.com>
2022-06-28 10:49:25 +0900

    refactor : 환경변수를 필드로 가지는 Property 클래스 작성

--- be/src/main/java/com/codesquad/issuetracker/user/application/oauth/OAuthProvider.java
+++ be/src/main/java/com/codesquad/issuetracker/user/application/oauth/OAuthProvider.java
@@ -1,10 +1,17 @@
 package com.codesquad.issuetracker.user.application.oauth;
 
 import com.codesquad.issuetracker.user.application.dto.OAuthUserInformation;
+import com.codesquad.issuetracker.user.domain.LoginType;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OAuthProvider {
 
+    LoginType getOAuthType();
+
+    String getRedirectUrl();
+
     OAuthUserInformation requestUserInformation(String code);
+
+
 }
