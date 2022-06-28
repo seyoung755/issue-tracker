a407ad6377dab8e3484bfa1d0342512c6169b8e7
seyeong <seyoung7555@naver.com>
2022-06-28 13:50:55 +0900

    feat: 마일스톤 조회/작성 api 구현

--- be/src/main/java/com/codesquad/issuetracker/milestone/presentation/dto/MilestoneResponseDto.java
+++ be/src/main/java/com/codesquad/issuetracker/milestone/presentation/dto/MilestoneResponseDto.java
@@ -1,9 +1,12 @@
 package com.codesquad.issuetracker.milestone.presentation.dto;
 
+import com.codesquad.issuetracker.milestone.domain.MilestoneInformation;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
 @Getter
 public class MilestoneResponseDto {
 
@@ -11,6 +14,15 @@ public class MilestoneResponseDto {
     private LocalDate dueDate;
     private String description;
     private double progressRate;
-    private int openCount;
-    private int closeCount;
+    private long openCount;
+    private long closeCount;
+
+    public MilestoneResponseDto(String name, LocalDate dueDate, String description, MilestoneInformation milestoneInformation) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.progressRate = milestoneInformation.getProgressRate();
+        this.openCount = milestoneInformation.getOpenCount();
+        this.closeCount = milestoneInformation.getCloseCount();
+    }
 }
--- be/src/main/java/com/codesquad/issuetracker/milestone/presentation/dto/MilestonesResponseDto.java
+++ be/src/main/java/com/codesquad/issuetracker/milestone/presentation/dto/MilestonesResponseDto.java
@@ -1,11 +1,17 @@
 package com.codesquad.issuetracker.milestone.presentation.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@NoArgsConstructor
 @Getter
 public class MilestonesResponseDto {
 
     private List<MilestoneResponseDto> milestones;
+
+    public MilestonesResponseDto(List<MilestoneResponseDto> milestones) {
+        this.milestones = milestones;
+    }
 }
