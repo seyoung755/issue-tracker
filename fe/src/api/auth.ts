96494c8693c77c7b80fb3a76d2d88abb04904b88
yunho <kimyouknow@naver.com>
2022-06-29 11:02:16 +0900

    Feat: github OAuth url 및 token요청 api
    Co-authored-by: Geon <ldldz@users.noreply.github.com>

--- fe/src/api/auth.ts
+++ fe/src/api/auth.ts
@@ -1,12 +1,19 @@
 import instance from '@/api/core';
-import { USER_API } from '@/constant/api';
 
-// TODO: url USER_API로 변경
 const authApi = {
-  requestGithubOAuth() {
+  getGithubOAuthUrl() {
     return instance({
       url: 'http://43.200.37.159/oauth/github/login',
       method: 'get',
+    });
+  },
+  getGithubOAuthToken(code: string) {
+    return instance({
+      url: 'http://43.200.37.159/oauth/github/callback',
+      method: 'get',
+      params: {
+        code,
+      },
     });
   },
 };
