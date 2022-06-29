5d7f18ad09a797178f5176a1cd8ae757a6f59307
함석호 <kalejuice02@gmail.com>
2022-06-30 02:00:38 +0900

    feat : 댓글 CRUD 기능 구현 및 예외 타입 정의

--- be/src/main/java/com/codesquad/issuetracker/comment/domain/Comment.java
+++ be/src/main/java/com/codesquad/issuetracker/comment/domain/Comment.java
@@ -3,9 +3,13 @@ package com.codesquad.issuetracker.comment.domain;
 import com.codesquad.issuetracker.common.domain.BaseEntity;
 import com.codesquad.issuetracker.issue.domain.Issue;
 import com.codesquad.issuetracker.user.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@NoArgsConstructor
+@Getter
 @Entity
 public class Comment extends BaseEntity {
     @Id
@@ -19,4 +23,18 @@ public class Comment extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Issue issue;
+
+    public Comment(String content, User user, Issue issue) {
+        this.content = content;
+        this.user = user;
+        this.issue = issue;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void delete() {
+        changeDeleted(false);
+    }
 }
