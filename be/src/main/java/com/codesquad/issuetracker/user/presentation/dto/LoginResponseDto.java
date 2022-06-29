3500678b48f4bdb4a53d4968f15fad2b88fbc8b0
seyeong <seyoung7555@naver.com>
2022-06-29 11:05:22 +0900

    refactor: oauth login 흐름 변경
    - body로 token을 전달한다.

--- be/src/main/java/com/codesquad/issuetracker/user/presentation/dto/LoginResponseDto.java
+++ be/src/main/java/com/codesquad/issuetracker/user/presentation/dto/LoginResponseDto.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 public class LoginResponseDto {
 
     private final String accessToken;
+    private final String refreshToken;
 
-    public LoginResponseDto(String accessToken) {
+    public LoginResponseDto(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
     }
 }
