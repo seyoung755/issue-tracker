0859f4f5c88cbf2155119ac8837a1c584e9fac2b
seyeong <seyoung7555@naver.com>
2022-06-29 22:02:55 +0900

    feat: 역직렬화를 위한 setter 추가

--- be/src/main/java/com/codesquad/issuetracker/issue/presentation/dto/FilteringCondition.java
+++ be/src/main/java/com/codesquad/issuetracker/issue/presentation/dto/FilteringCondition.java
@@ -2,14 +2,20 @@ package com.codesquad.issuetracker.issue.presentation.dto;
 
 import com.codesquad.issuetracker.issue.domain.IssueStatus;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class FilteringCondition {
 
     private IssueStatus issueStatus;
     private String labelName;
     private String milestoneName;
-    private String author;
+    private Long authorId;
     private String assignee;
     private String commentAuthor;
+
+    public void setIssueStatus(String issueStatus) {
+        this.issueStatus = IssueStatus.from(issueStatus);
+    }
 }
